65e97f6613bb6eadc099e91690304a21056215f0
gluemstr1010 <beantotemic@gmail.com>
2023-06-13 20:54:12 +0200

    nothing much added

--- klient/klient.c
+++ klient/klient.c
@@ -5,9 +5,34 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <unistd.h>
 
 #define SIZE 1024
+
+struct mmsghdr
+{
+    struct msghdr msg_hdr;
+    unsigned int msg_len;
+};
+
+long get_file_size(char *filename) {
+    FILE *fp = fopen(filename, "r");
+
+    if (fp==NULL)
+        return -1;
+
+    if (fseek(fp, 0, SEEK_END) < 0) {
+        fclose(fp);
+        return -1;
+    }
+
+    long size = ftell(fp);
+    
+    fclose(fp);
+    return size;
+}
+
 void send_file(FILE *file, int net_socket)
 {
     int n;
@@ -60,10 +85,13 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in server_address;
+    struct sockaddr_in server_address, new_addr;
+    socklen_t addr_size;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(7070);
     server_address.sin_addr.s_addr = INADDR_ANY;
+
+   
 
     int conn_status = connect(net_socket, (struct sockaddr *) &server_address, sizeof(server_address));
     if( conn_status == -1 )
@@ -79,7 +107,19 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    long fileSize = get_file_size(filename);
     send_file(file, net_socket);
+    // addr_size = sizeof(new_addr);
+    // int acc = accept(net_socket, (struct sockaddr*)&new_addr,&addr_size);
+    // char server_res[256];
+    // int b = recv(acc,&server_res,sizeof(server_res),0);
+    // if(b > 0)
+    // {
+    //     printf("%s",server_res);
+    // }else
+    // {
+    //     printf("\nFile sent successfully.\n");
+    // }
 
     printf("\nClosing connection ... \n");
     close(net_socket);
--- server/server.c
+++ server/server.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <stdbool.h>
 #include <string.h>
-#include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include "word.c"
 
 #define SIZE 1024
 
+char* itoa(int num, char* buffer, int base)   
+{  
+int current = 0;  
+    if (num == 0) 
+    {  
+        buffer[current++] = '0';  
+        buffer[current] = '\0';  
+        return buffer;  
+    }  
+    int num_digits = 0;  
+    if (num < 0) 
+    {  
+        if (base == 10) 
+        {  
+            num_digits ++;  
+            buffer[current] = '-';  
+            current ++;  
+            num *= -1;  
+        }  
+            else  
+            return NULL;  
+    }  
+    num_digits += (int)floor(log(num) / log(base)) + 1;  
+    while (current < num_digits)   
+    {  
+        int base_val = (int) pow(base, num_digits-1-current);  
+        int num_val = num / base_val;  
+        char value = num_val + '0';  
+        buffer[current] = value;  
+        current ++;  
+        num -= base_val * num_val;  
+    }  
+buffer[current] = '\0';  
+return buffer;  
+}  
 
 bool file_exists(const char *filename)
 {
@@ -32,6 +68,7 @@ void write_file(int new_socket)
     const char* filename = malloc(32);
     filename = word();
     char buffer[SIZE];
+    long fileSize;
     while(true)
     {
         if(file_exists(filename))
@@ -43,18 +80,7 @@ void write_file(int new_socket)
             break;
         }
     }
-    // int g = 0;
-    // while(true)
-    // { 
-    //     b = recv(new_socket,buffer,SIZE,0);
-    //     if(b > 0)
-    //     {
-    //         g+=b;
-    //     }else
-    //     {
-    //         break;
-    //     }
-    // }
+           
     file = fopen(filename,"w");
         while(true)
         {
@@ -67,7 +93,6 @@ void write_file(int new_socket)
             fprintf(file,"%s",buffer);
             bzero(buffer,SIZE);
         }
-    
     
     return;
 }
